8d34897b45687213aa22d1fc4a9ec972f57671cd
mbclabvit <163407458+mbclabvit@users.noreply.github.com>
2024-03-14 16:01:21 +0530

    Update single_line.pl

--- single_line.pl
+++ single_line.pl
@@ -1,10 +1,9 @@
 #!/usr/bin/perl
 #use strict;
-#open (FILE,"final_name.txt") || die "can't able to open the file \n";
-##chomp $file_name;
-my $file = 'test.fasta';
-open(IN,$file) || die ("Cannot open file");
-open(OUT, ">final/$file.txt") || die "can't able to write the file \n";
+open (FILE,"final_name.txt") || die "can't able to open the file \n";
+chomp $file_name;
+open(IN,$file_name) || die ("Cannot open file");
+open(OUT, ">final/$file_name.txt") || die "can't able to write the file \n";
 
 my $line = <IN>; 
 print OUT $line;
